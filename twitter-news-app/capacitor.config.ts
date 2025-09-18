42dc6e8ee810b259a03d82a45511cb9fb4a75559
TheRealNightmare <nnahid929@gmail.com>
2025-09-19 03:16:25 +0600

    update 3.0

--- twitter-news-app/capacitor.config.ts
+++ twitter-news-app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'twitter-news-app',
+  appName: 'TWT',
   webDir: 'dist',
   plugins: {
     StatusBar: {
